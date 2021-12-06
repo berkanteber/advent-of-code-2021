64b9d610c0058754a9824a30e4ef63f753d558e9
Berkan Teber <berkan@berkanteber.com>
2021-12-06 09:05:03 +0300

    Add day 6

---
+++ day_06/part_2.py
@@ -0,0 +1,26 @@
+import os
+
+input_path = os.path.join(os.path.dirname(__file__), "input.txt")
+
+with open(input_path) as f:
+    data = f.read()
+
+
+def solve(data: str) -> int:
+    internal_days = {day: 0 for day in range(9)}
+    for day in data.split(","):
+        internal_days[int(day)] += 1
+
+    for _ in range(256):
+        internal_days = {
+            **{day: internal_days[day + 1] for day in {0, 1, 2, 3, 4, 5, 7}},
+            6: internal_days[0] + internal_days[7],
+            8: internal_days[0],
+        }
+
+    return sum(internal_days.values())
+
+
+assert solve("3,4,3,1,2") == 26984457539
+
+print(solve(data))  # 1721148811504
