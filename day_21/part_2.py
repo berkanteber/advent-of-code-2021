b67d4ad1bb132a61d484d4c03948091290d03a8f
Berkan Teber <berkan@berkanteber.com>
2021-12-30 10:56:24 +0300

    Add incorrect solution for day 21 part 2
    Currently, below operations seem to work:
    - Create up to 3 universes (do not create if a player wins) in each turn
    - Exhaust all universes at the end

---
+++ day_21/part_2.py
@@ -0,0 +1,64 @@
+import os
+
+input_path = os.path.join(os.path.dirname(__file__), "input.txt")
+
+with open(input_path) as f:
+    data = f.read()
+
+
+def solve(data: str) -> int:
+    player1_str, player2_str = data.splitlines()
+    initial_positions = (int(player1_str.split()[-1]), int(player2_str.split()[-1]))
+
+    total_score = 0
+    universes = {total_score: {(initial_positions, (0, 0), 1): 1}}
+    wins = {1: 0, 2: 0}
+
+    current_score = 0
+    while universes:
+        while current_score not in universes:
+            current_score += 1
+
+        state, count = universes[current_score].popitem()
+
+        for dice in (1, 2, 3):
+            (position1, position2), (score1, score2), turn = state
+
+            if turn == 1:
+                position1 = (position1 + dice - 1) % 10 + 1
+                score1 += position1
+                if score1 >= 21:
+                    wins[1] += count
+                    continue
+            else:
+                position2 = (position2 + dice - 1) % 10 + 1
+                score2 += position2
+                if score2 >= 21:
+                    wins[2] += count
+                    continue
+
+            total_score = score1 + score2
+            if total_score not in universes:
+                universes[total_score] = {}
+
+            new_state = ((position1, position2), (score1, score2), 3 - turn)
+
+            if new_state not in universes[total_score]:
+                universes[total_score][new_state] = 0
+
+            universes[total_score][new_state] += count
+
+        if not universes[current_score]:
+            universes.pop(current_score)
+
+    return max(wins.values())
+
+
+example_data = """\
+Player 1 starting position: 4
+Player 2 starting position: 8
+"""
+
+assert solve(example_data) == 444356092776315  # Assertion Fails
+
+print(solve(data))  # Wrong Answer
